b3859ae148cfb3947847d6d7dcc880f91cc02221
martin.frank <martin.frank.private@gmail.com>
2019-04-01 09:36:59 +0200

    Initial commit

---
+++ app/src/main/java/de/elite/games/android/rpglibdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package de.elite.games.android.rpglibdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
